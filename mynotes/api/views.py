bab539af944bf2e9f2d28813c914015447705792
Mahek Unnisa <mahekunnisa786@gmail.com>
2023-01-15 19:34:26 +0530

    Notes by id API

--- mynotes/api/views.py
+++ mynotes/api/views.py
@@ -49,3 +49,9 @@ def getNotes(request):
     notes = Note.objects.all()
     serializer = NoteSerializer(notes, many=True)
     return Response(serializer.data)
+    
+@api_view(['GET'])
+def getNote(request,pk):
+    notes=Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
